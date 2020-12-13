c8dfb2e8bc7ab2a360789bd4677850275ff62d26
Andrew M <andrewlmanley@gmail.com>
2020-12-13 18:23:30 -0500

    Added error message

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,18 @@ import Route from '@ember/routing/route';
 import ENV from 'movietrends/config/environment';
 
 export default class IndexRoute extends Route {
+  get hasData() {
+    return this.model && this.model?.length > 0;
+  }
   async model() {
-    const response = await fetch(`${ENV.THEMOVIEDB_BASE_API_URL}/trending/movie/day?api_key=${ENV.THEMOVIEDB_API_KEY}`);
-    const data = await response.json();
-    return data.results;
+    let movies = [];
+    try {
+      const response = await fetch(`${ENV.THEMOVIEDB_BASE_API_URL}/trending/movie/day?api_key=${ENV.THEMOVIEDB_API_KEY}`);
+      const data = await response.json();
+      movies = data.results;
+    } catch(error) {
+      console.error(error);
+    }
+    return movies;
   }
 }
